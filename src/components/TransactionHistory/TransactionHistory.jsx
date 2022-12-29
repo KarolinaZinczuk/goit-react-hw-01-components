702bf2c42922fd343dcba63dbca8a30ef459b722
KarolinaZinczuk <104589501+KarolinaZinczuk@users.noreply.github.com>
2022-12-29 22:33:24 +0100

    Update TransactionHistory.jsx

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -1,9 +1,37 @@
 import PropTypes from "prop-types";
 
-const TransactionHistory = ({ items });
+const TransactionHistory = ({ items }) => {
+    return (
+        <table class="transaction-history">
+            <thead>
+                <tr>
+                    <th>Type</th>
+                    <th>Amount</th>
+                    <th>Currency</th>
+                </tr>
+            </thead>
+            <tbody>
+                {items.map(({ id, type, amount, currency }) => (
+                    <tr key={id}>
+                        <td>{type}</td>
+                        <td>{amount}</td>
+                        <td>{currency}</td>
+                    </tr>
+                ))}               
+            </tbody>
+        </table>
+    );
+};
 
 TransactionHistory.propTypes = {
-    items: PropTypes.string
+    items: PropTypes.arrayOf(
+        PropTypes.shape({
+            id: PropTypes.string,
+            type: PropTypes.string,
+            amount: PropTypes.number,
+            currency: PropTypes.string,
+        })
+    ),
 };
 
 export default TransactionHistory;
