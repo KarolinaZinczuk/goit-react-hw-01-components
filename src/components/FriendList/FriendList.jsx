88258bac185a8bc351bd00dce3115b55a7b4af1c
KarolinaZinczuk <104589501+KarolinaZinczuk@users.noreply.github.com>
2022-12-30 10:04:42 +0100

    Update FriendList.jsx

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,10 +1,10 @@
 import PropTypes from "prop-types";
 import FriendListItem from "./FriendListItem";
-import styles from "./FriendList.module";
+import styles from "./FriendList.module.css";
 
 const FriendList = ({ friends }) => {
     return (
-        <ul className={styles.friend-list}>
+        <ul className={styles.friendList}>
             {friends.map(({ avatar, name, isOnline, id}) =>
             <FriendListItem
                 avatar={avatar}
